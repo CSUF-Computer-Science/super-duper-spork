fe75a35c80fca6c1060ad5dbd5ea091939782790
staceyfras <32045253+staceyfras@users.noreply.github.com>
2018-10-03 12:01:57 -0700

    (:

--- sporkify/backend/admin.py
+++ sporkify/backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Open_Product_Code)
+admin.site.register(Slot_size)
+admin.site.register(Vendor)
+admin.site.register(Inventory)
+admin.site.register(Sale_Site)
+admin.site.register(Condition)
+admin.site.register(Sale)
+admin.site.register(Product_Type)
+admin.site.register(Employee)
+admin.site.register(Shift)
+admin.site.register(Shipment)
