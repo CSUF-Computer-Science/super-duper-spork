affe4d4fc39217303375ae26e9d1c1b53abf9f4b
Joshua Ferrara <scjosh2@gmail.com>
2018-10-13 13:48:35 -0700

    Initial inventory work with templating.

--- sporkify/sporkify/urls.py
+++ sporkify/sporkify/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from frontend import views
+
 urlpatterns = [
+    path('', views.inventory),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
 ]
