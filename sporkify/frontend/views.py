1c1154e0feb0a1dd7ab225e17a2979ae5d4896d1
David Feinzimer <dfeinzimer@gmail.com>
2018-10-15 14:25:03 -0700

    Adds dynamic form element generation

--- sporkify/frontend/views.py
+++ sporkify/frontend/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from backend.models import Inventory
+from backend.models import Vendor
+from backend.models import Sale_Site
 
 @login_required
 def inventory(request):
     return render(request, 'inventory.html', {
-        "items": Inventory.objects.all()
+        "items": Inventory.objects.all(),
+        "vendors": Vendor.objects.all(),
+        "channels": Sale_Site.objects.all()
     })
