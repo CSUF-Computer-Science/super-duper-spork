45e21327cb1fbea47e90e32a148a4279ca2d47d2
staceyfras <32045253+staceyfras@users.noreply.github.com>
2018-10-17 10:48:53 -0700

    Update views.py

--- sporkify/frontend/views.py
+++ sporkify/frontend/views.py
@@ -4,11 +4,15 @@ from django.contrib.auth.decorators import login_required
 from backend.models import Inventory
 from backend.models import Vendor
 from backend.models import Sale_Site
+from backend.models import Employee
+from backend.models import Shift
 
 @login_required
 def inventory(request):
     return render(request, 'inventory.html', {
         "items": Inventory.objects.all(),
         "vendors": Vendor.objects.all(),
-        "channels": Sale_Site.objects.all()
+        "channels": Sale_Site.objects.all(),
+        "employee": Employee.objects.all(),
+        "shift": Shift.objects.all()
     })
