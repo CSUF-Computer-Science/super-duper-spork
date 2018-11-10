9bb58a18f931af0c22a4272a746c7e39bc675766
David Feinzimer <dfeinzimer@gmail.com>
2018-11-10 14:22:32 -0800

    Inventory table formatting

--- sporkify/frontend/templates/inventory.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-function hideAddItem() {
-        var addItemDiv = document.getElementById("add_item").style;
-    addItemDiv.display = (addItemDiv.display === "none") ? "block" : "none";
-}
